15d996adcf5a42212c993be66ff0fb3431438271
Josh <josh.espinas@xero.com>
2020-02-26 01:11:10 +1300

    Testing new change with set up pipeline

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the YGO classifier!
+          Welcome to the YGO classifier! NEW CHANGE
         </p>
         <a
           className="App-link"
