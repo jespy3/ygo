13c27ae35314e07448216ca2a6133d8863992af6
Josh <josh.espinas@xero.com>
2020-02-26 01:35:30 +1300

    Triggering again for testing why change didn't come thorugh

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the YGO classifier! NEW CHANGE
+          Welcome to the YGO classifier! PLEASE SHOW ME THE NEW CHANGE
         </p>
         <a
           className="App-link"
