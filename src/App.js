1a559feae54fa83b2b1f1ad3870257b1c9efe6ed
Josh <josh.espinas@xero.com>
2020-03-28 14:00:50 +1300

    Words the under construction sentence better

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the YGO classifier! Site under construction.
+          Welcome to the YGO classifier! This site under construction.
         </p>
         <Basic></Basic>
         <ReferenceLine />
