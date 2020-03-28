e8627d93617036021aa18ce4708fdeb9aca5254e
Josh <josh.espinas@xero.com>
2020-03-28 14:43:59 +1300

    Turns link formatting into a function

--- src/components/referenceLine.js
+++ src/components/referenceLine.js
@@ -3,18 +3,21 @@ import React from 'react';
 const courseCheckpoint="https://reactjs.org/docs/introducing-jsx.html";
 const chapterNumber = 2;
 const chapterName = "Introducing JSX";
-const link = (
-  <a
-    className="App-link"
-    href={courseCheckpoint}
-  >
-    Chapter {chapterNumber}: {chapterName}
-  </a>
-);
+
+function LinkComponent(link) {
+  return (
+    <a
+      className="App-link"
+      href={link}
+    >
+      Chapter {chapterNumber}: {chapterName}
+    </a>
+  );
+}
 
 const element = (
   <div>
-    Currently working through React's Getting Started Course (Main Concepts) - {link}
+    Currently working through React's Getting Started Course (Main Concepts) - {LinkComponent(courseCheckpoint)}
   </div>
 );
 
