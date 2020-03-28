5835914bbf9bc8ecc2aee303c35662c473f85fbf
Josh <josh.espinas@xero.com>
2020-03-28 14:52:28 +1300

    Creates and abstracts out a welcome component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import './App.css';
 
 import Basic from './components/basic';
 import ReferenceLine from './components/referenceLine';
+import Welcome from './components/welcome';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This site under construction.
-        </p>
+        <Welcome/>
         <Basic/>
         <ReferenceLine/>
       </header>
---
+++ src/components/welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const element = (
+  <p>
+    Welcome! This site under construction.
+  </p>
+);
+
+function Welcome() {
+  return (
+    element
+  );
+}
+
+export default Welcome;
