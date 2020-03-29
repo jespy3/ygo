5ac6b34a2da3287aa74ebdd1e3272cdb11918fc1
Josh <josh.espinas@xero.com>
2020-03-29 16:11:41 +1300

    Updates reference that I'm up to chapter 5

--- src/components/referenceLine.js
+++ src/components/referenceLine.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const courseCheckpoint="https://reactjs.org/docs/introducing-jsx.html";
-const chapterNumber = 4;
-const chapterName = "Components and Props";
+const chapterNumber = 5;
+const chapterName = "State and Lifecycle";
 
 function LinkComponent(link) {
   return (
